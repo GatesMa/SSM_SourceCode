289646ff6098f79695ea40cb0912a757d338acde
GatesMa <gatesma@foxmail.com>
2020-02-03 20:37:14 +0800

    Servlet3注解

---
+++ servlet3/src/com/gatesna/servlet/HelloServlet.java
@@ -0,0 +1,38 @@
+package com.gatesna.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Copyright (C), 2020
+ * FileName: HelloServlet
+ * Author:   Marlon
+ * Email: gatesma@foxmail.com
+ * Date:     2020/2/3 20:29
+ * Description:
+ */
+@WebServlet(value = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        //super.doGet(req, resp);
+//        System.out.println(Thread.currentThread()+" start...");
+//        try {
+//            sayHello();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        resp.getWriter().write("hello...");
+//        System.out.println(Thread.currentThread()+" end...");
+    }
+
+    public void sayHello() throws Exception{
+        System.out.println(Thread.currentThread()+" processing...");
+        Thread.sleep(3000);
+    }
+}
