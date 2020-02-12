166cca2ae2019e9f69bf002767d9c2a17be9a965
GatesMa <gatesma@foxmail.com>
2020-02-12 12:43:20 +0800

    upd

---
+++ SpringBoot_Source/src/main/java/com/gatesma/HelloWorldApplication.java
@@ -0,0 +1,12 @@
+package com.gatesma;
+
+/**
+ * Copyright (C), 2020
+ * FileName: HelloWorldApplication
+ * Author:   Marlon
+ * Email: gatesma@foxmail.com
+ * Date:     2020/2/11 21:06
+ * Description:
+ */
+public class HelloWorldApplication {
+}
---
+++ SpringBoot_Source/src/main/java/com/gatesma/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.gatesma.controller;
+
+/**
+ * Copyright (C), 2020
+ * FileName: HelloController
+ * Author:   Marlon
+ * Email: gatesma@foxmail.com
+ * Date:     2020/2/11 21:08
+ * Description:
+ */
+public class HelloController {
+}
